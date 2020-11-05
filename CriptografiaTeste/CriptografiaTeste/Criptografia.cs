01030bf6701c5c26a33353e9ad464d4c8fe289d3
Nathan Vilela <nathanvs@outlook.com>
2020-11-05 08:58:47 -0300

    Atualização

---
+++ CriptografiaTeste/CriptografiaTeste/Criptografia.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CriptografiaTeste
+{
+    class Criptografia
+    {
+        public static byte[] Encrypt(string texto, byte[] key, byte[] IV)
+        {
+            if (texto == null || texto.Length <= 0)
+                throw new ArgumentNullException("texto");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = IV;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using(MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(texto);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        public static string Decrypt(byte[] TextoCriptografado, byte[] Key, byte[] IV)
+        {
+            if (TextoCriptografado == null || TextoCriptografado.Length <= 0)
+                throw new ArgumentNullException("TextoCriptografafo");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string textoDescriptografado =  null;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(TextoCriptografado))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            textoDescriptografado = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return textoDescriptografado;
+
+        }
+    }
+}
---
+++ CriptografiaTeste/CriptografiaTeste/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CriptografiaTeste
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                Console.Write("Digite um Texto para criptografia: ");
+                string texto = Console.ReadLine();
+                
+                
+                byte[] crypto = Criptografia.Encrypt(texto, aes.Key , aes.IV );
+                string textodescriptografado = Criptografia.Decrypt(crypto, aes.Key, aes.IV);
+
+                Console.WriteLine("Seu texto CRIPTOGRAFAFO: {0}", System.Text.Encoding.UTF8.GetString(crypto));
+                Console.WriteLine("Seu texto DESCRIPTOGRAFADO: {0}", textodescriptografado);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
